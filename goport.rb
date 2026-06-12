28ca8ec15c8909a5bd74ca0b20cb59b725f7a2b2
Muhammad <muhammadgo.deve@gmail.com>
2026-06-12 17:53:37 +0500

    Update formula to v0.1.1

--- goport.rb
+++ goport.rb
@@ -1,21 +1,34 @@
 class Goport < Formula
   desc "Expose localhost to the internet instantly"
   homepage "https://goport.uz"
-  version "0.1.0"
+  version "0.1.1"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/muhammad-deve/GoPort/releases/download/v0.1.0/goport-darwin-arm64"
-    sha256 "33129409de3874af77c7552bd472eb96201986e95133bd1ff621c68911e297ae"
-  else
-    url "https://github.com/muhammad-deve/GoPort/releases/download/v0.1.0/goport-darwin-amd64"
-    sha256 "9a458059ab00be54f80e1870b10e6be41aa4e8ad513e4322df3760f7f09fa7b4"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/muhammad-deve/GoPort/releases/download/v0.1.1/goport-darwin-arm64"
+      sha256 "949867aa51b98146abd5219d29ecee1c0e7fca54528a9f7c4be76bf04fb33a10"
+    else
+      url "https://github.com/muhammad-deve/GoPort/releases/download/v0.1.1/goport-darwin-amd64"
+      sha256 "2239287fbdc92843b009a0923fb79b9809d774ab6298a62e3cb6b5955a22f92d"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/muhammad-deve/GoPort/releases/download/v0.1.1/goport-linux-amd64"
+    sha256 "b1b354d0f5cf4828d72d374d9a235c40b02d53849a6487a87b704b6a50af87ff"
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "goport-darwin-arm64" => "goport"
-    else
-      bin.install "goport-darwin-amd64" => "goport"
+    on_macos do
+      if Hardware::CPU.arm?
+        bin.install "goport-darwin-arm64" => "goport"
+      else
+        bin.install "goport-darwin-amd64" => "goport"
+      end
+    end
+
+    on_linux do
+      bin.install "goport-linux-amd64" => "goport"
     end
   end
 
